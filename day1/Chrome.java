000ab11804f7f09ee63169237f5960a2c3c744b5
Kishore619-work <kishorevenkatachalam26@gmail.com>
2025-03-02 14:48:33 +0530

    Add files via upload
    Day1

---
+++ day1/Chrome.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Chrome {
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear= 2008;
+		char shortcutKey='C';
+		System.out.println("The Chrome version is: "+version+"\nThe developer name is:"+developer+"\nBeta Version:"+isBeta+"\nThe release year is:"+releaseYear+
+				"\nThe shortcut is:"+shortcutKey);
+	}
+
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Firefox {
+public static void main(String[] args) {
+	float browserVersion =100.2f;
+	String browserName = "firefox";
+	boolean isVisible = true;
+	int releaseYear = 1998;
+	char browserLogo = 'f';
+	System.out.println("Browser version = " + browserVersion + "\nBrowser name = " + browserName + "\nBrowser visibility"+ isVisible + "\nRelease year = " + releaseYear +
+			"\nBrowser Logo = "+ browserLogo);
+}
+}
